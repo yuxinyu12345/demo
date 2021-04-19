9063cb09e4f200a88fe7f8daa6a2ed256227e751
晓欲望 <411359274@qq.com>
2021-04-19 11:34:13 +0800

    mockJs的小demo

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { reqSwipes } from '@/api/mockDemo'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    this.getSwipes()
+  },
+  methods: {
+    async getSwipes() {
+      const res = await reqSwipes()
+      console.log(res);
+    }
+  }
+}
+</script>
